bdf061dedbd8b28f65171fc0e8c272bd5252bb5a
Hasitha Prasanga Abewardana Houpe Mudiyanselage <hasi.prasanga@gmail.com>
2025-08-11 10:06:58 +1200

    Route home page from logo and webbapp name

--- frontend/src/shared/components/Header.js
+++ frontend/src/shared/components/Header.js
@@ -1,12 +1,24 @@
+import { Link } from "react-router-dom";
+
 // Header component for shared navigation
 function Header() {
   return (
     <header>
-      <img src="tourism-pulse-nz-logo.png" alt="TourismPulseNZ Logo" />
-      <div>
-        <h1>TourismPulseNZ</h1>
-        <p>A Smart Web-Based Tourism Management Platform</p>
-      </div>
+      <Link
+        to="/"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          textDecoration: "none",
+          color: "inherit",
+        }}
+      >
+        <img src="tourism-pulse-nz-logo.png" alt="TourismPulseNZ Logo" />
+        <div>
+          <h1>TourismPulseNZ</h1>
+          <p>A Smart Web-Based Tourism Management Platform</p>
+        </div>
+      </Link>
     </header>
   );
 }
