b0a3ff63fb20ed52d54507ad51ae8423b64a330b
Hasitha Prasanga Abewardana Houpe Mudiyanselage <hasi.prasanga@gmail.com>
2025-10-09 12:18:43 +1300

    Fix issues in guest mode

--- backend/destination-service/src/routes/destinationRoutes.ts
+++ backend/destination-service/src/routes/destinationRoutes.ts
@@ -11,7 +11,8 @@ import { authenticate, authorize } from "../middleware/authMiddleware";
 const router = Router();
 
 // Public access
-router.get("/v1/destinations/public", getDestinations); // Public list with thumbnails
+router.get("/v1/destinations/public", getDestinations);
+router.get("/v1/destinations/:id/public", getDestinationById);
 
 // Protected routes
 router.use(authenticate);
